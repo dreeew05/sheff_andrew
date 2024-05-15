08fc3280ba5a065974e205b18f330a047544f548
Glen Andrew <gcbulaong@up.edu.ph>
2024-05-15 20:21:55 +0800

    Move to another folder

---
+++ lib/common/utils/app_painter.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppPainter {
+  getCardColor() {
+    return const Color.fromRGBO(241, 241, 241, 1);
+  }
+
+  getCardTextColor() {
+    return const Color.fromRGBO(72, 72, 72, 1);
+  }
+}
