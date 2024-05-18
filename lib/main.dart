e55cedef2c3d9e2b544313ac2386d89dc6fe6107
Glen Andrew <gcbulaong@up.edu.ph>
2024-05-18 13:48:44 +0800

    Setup Implementation
    - Redirect to home if user is signed in. Otherwise, go to sign in page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:sheff_andrew/screens/home-page/home_page.dart';
+import 'package:sheff_andrew/app_navigator.dart';
 import 'package:sheff_andrew/screens/signup/signin_page.dart';
-import 'package:sheff_andrew/screens/signup/signup_page.dart';
-import 'package:sheff_andrew/screens/home-page/components/recipe_carousel.dart'; 
-import 'package:sheff_andrew/screens/community_page/community.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => SignInPage(),
-      '/homepage': (context) => HomePage(),
-      '/signup': (context) => SignUpPage(),
-      '/recipe_carousel': (context) => RecipeCarousel(), 
-      'recipe_carousel/community': (context) => CommunityPage(), 
-    },
-  ));
+  runApp(const MyApp());
 }
 
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => StreamBuilder<User?>(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return const AppNavigator();
+                } else {
+                  // User is not signed in
+                  return SignInPage();
+                }
+              },
+            ),
+      },
     );
   }
 }
