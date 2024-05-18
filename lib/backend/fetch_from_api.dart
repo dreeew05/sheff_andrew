b4a839f4ab8b4522d9049e5405979e4cc8c1b7f9
Glen Andrew <gcbulaong@up.edu.ph>
2024-05-19 02:36:43 +0800

    Generative Search of Recipe

--- lib/backend/fetch_from_api.dart
+++ lib/backend/fetch_from_api.dart
@@ -3,7 +3,8 @@
   Purpose of this file: Fetch from Edamam API to Firebase
 */
 
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class FetchFromApi {
@@ -18,12 +19,13 @@ class FetchFromApi {
     return _appKey;
   }
 
-  Future<http.Response> fetchRecipes(String query) {
+  Future<http.Response> queryRequest(String query) {
+    query = query.replaceAll(' ', '%20');
     return http.get(
       Uri.parse('''
         https://api.edamam.com/api/recipes/v2
         ?type=public
-        &q=Chicken%20Margarita
+        &q=$query
         &app_id=${getAppId()}
         &app_key=${getAppKey()}
       '''
@@ -32,25 +34,58 @@ class FetchFromApi {
     );
   }
 
-  Future<QuerySnapshot<Object?>> getRecipes() async {
-    final FirebaseFirestore firestore = FirebaseFirestore.instance;
-    CollectionReference recipesCollection = firestore.collection('recipes');
-    return await recipesCollection.get();
+  Future<Map<String, dynamic>> fetchRecipes(String recipeName) async {
+    final apiClient = FetchFromApi();
+    final response = await apiClient.queryRequest(recipeName);
+    List<Map<String, dynamic>> recipeList = [];
+    Map<String, dynamic> dataToBeReturned = {};
+
+    // Connection was successful
+    if (response.statusCode == 200) {
+      // Process the data
+      var jsonData = jsonDecode(response.body);
+      // print(jsonData['hits'].isEmpty);
+
+      if (jsonData['hits'].isEmpty) {
+        dataToBeReturned = {
+          "status": "no_results",
+        };
+        return dataToBeReturned;
+      }
+
+      for (var recipe in jsonData["hits"]) {
+        recipeList.add({
+          "label": recipe["recipe"]["label"],
+          "image": recipe["recipe"]["image"],
+          "dietLabels": recipe["recipe"]["dietLabels"],
+          "healthLabels": recipe["recipe"]["healthLabels"],
+          "cautions": recipe["recipe"]["cautions"],
+          "ingredients": recipe["recipe"]["ingredients"],
+          "calories": recipe["recipe"]["calories"],
+          "totalTime": recipe["recipe"]["totalTime"],
+          "cuisineType": recipe["recipe"]["cuisineType"],
+          "mealType": recipe["recipe"]["mealType"],
+          "totalNutrients": recipe["recipe"]["totalNutrients"],
+          "tags": recipe["recipe"]["tags"],
+        });
+        dataToBeReturned = {
+          "recipeList": recipeList,
+          "status": "success",
+        };
+      }
+    } else {
+      // Connection failed
+      dataToBeReturned = {
+        "status": "error",
+      };
+    }
+    return dataToBeReturned;
   }
 }
 
+// Example usage
 void main() async {
   final apiClient = FetchFromApi();
-  // final response = await apiClient.fetchRecipes("Pasta");
-
-  print(apiClient.getRecipes());
-
-  // Handle the response here
-  // if (response.statusCode == 200) {
-  //   // Process the data (e.g., parse JSON)
-  //   print(response.body);
-  // } else {
-  //   // Handle error
-  //   print('Error: ${response.statusCode}');
-  // }
+  final response = await apiClient.fetchRecipes("Margarita");
+  print(response);
 }
---
+++ lib/screens/add_recipe/components/generative_search.dart
@@ -0,0 +1,144 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sheff_andrew/backend/fetch_from_api.dart';
+import 'package:sheff_andrew/screens/add_recipe/components/relevant_recipe_card.dart';
+
+class GenerativeSearch extends StatefulWidget {
+  const GenerativeSearch({super.key});
+
+  @override
+  GenerativeSearchState createState() => GenerativeSearchState();
+}
+
+class GenerativeSearchState extends State<GenerativeSearch> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _searchController = TextEditingController();
+
+  bool _showResultsFromApi = false;
+  bool _showNoResultFound = false;
+  List<Map<String, dynamic>> _recipeList = [];
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void fetchRelevantRecipes() async {
+    final apiClient = FetchFromApi();
+    final response = await apiClient.fetchRecipes(_searchController.text);
+    setState(() {
+      switch (response["status"]) {
+        case "success":
+          _showResultsFromApi = true;
+          _recipeList = response["recipeList"];
+          break;
+        case "no_results":
+          _showNoResultFound = true;
+          _showResultsFromApi = false;
+          _recipeList = [];
+          break;
+        case "error":
+          _showResultsFromApi = false;
+          _recipeList = [];
+          break;
+
+        default:
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Container(
+            margin: const EdgeInsets.only(
+              top: 30,
+              bottom: 30,
+              left: 20,
+              right: 20,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          style: GoogleFonts.poppins(),
+                          controller: _searchController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a recipe name';
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              errorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                color: Color.fromARGB(255, 214, 44, 32),
+                                width: 1.0,
+                              )),
+                              labelText: "Enter Recipe Name"),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 10),
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              fetchRelevantRecipes();
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 20)),
+                  Visibility(
+                      visible: _showResultsFromApi,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Relevant Recipes',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 24, fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 20),
+                          CarouselSlider(
+                              options: CarouselOptions(
+                                height: 200,
+                                aspectRatio: 16 / 9,
+                                viewportFraction: 1,
+                                initialPage: 0,
+                                enableInfiniteScroll: false,
+                                scrollDirection: Axis.horizontal,
+                              ),
+                              items: _recipeList
+                                  .map((recipe) => RelevantRecipeCard(
+                                      label: recipe['label'] as String,
+                                      image: recipe['image'] as String,
+                                      totalTime: recipe['totalTime'] as double))
+                                  .toList())
+                        ],
+                      )),
+                  Visibility(
+                      visible: _showNoResultFound,
+                      child: const Text('No results found')),
+                ],
+              ),
+            )));
+  }
+}
