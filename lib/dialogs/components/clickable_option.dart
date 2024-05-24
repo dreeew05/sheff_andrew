0126cb00702f32add0a910f6927422861901f867
Glen Andrew <gcbulaong@up.edu.ph>
2024-05-25 02:05:11 +0800

    Dynamic addition of images

--- lib/dialogs/components/clickable_option.dart
+++ lib/dialogs/components/clickable_option.dart
@@ -13,27 +13,42 @@ import 'package:sheff_andrew/providers/recipe_form_provider.dart';
 class ClickableOption extends StatelessWidget {
   final String optionText;
   final String orientation;
-  const ClickableOption(
-      {super.key, required this.optionText, required this.orientation});
+  final String imageType;
+
+  const ClickableOption({
+    super.key,
+    required this.optionText,
+    required this.orientation,
+    required this.imageType,
+  });
 
   RoundedRectangleBorder getOrientation() {
     switch (orientation) {
       case 'top':
         return const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(16.0),
-          topRight: Radius.circular(16.0),
-        ));
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16.0),
+            topRight: Radius.circular(16.0),
+          ),
+        );
       case 'bottom':
         return const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(16.0),
-          bottomRight: Radius.circular(16.0),
-        ));
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(16.0),
+            bottomRight: Radius.circular(16.0),
+          ),
+        );
       default:
         // Middle
         return const RoundedRectangleBorder();
     }
+  }
+
+  void _setPickedImage(BuildContext context, ImageSource source) {
+    final providerReader = context.read<RecipeFormProvider>();
+    imageType == 'recipe'
+        ? providerReader.setRecipePickedImage(source)
+        : providerReader.setIngredientPickedImage(source);
   }
 
   @override
@@ -44,27 +59,22 @@ class ClickableOption extends StatelessWidget {
       height: 70,
       width: MediaQuery.of(context).size.width,
       child: ElevatedButton(
-          onPressed: () {
-            if (optionText == 'Camera') {
-              context
-                  .read<RecipeFormProvider>()
-                  .setPickedImage(ImageSource.camera);
-            } else {
-              context
-                  .read<RecipeFormProvider>()
-                  .setPickedImage(ImageSource.gallery);
-            }
-            Navigator.pop(context);
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: appPainter.getDialogOptionColor(),
-            textStyle: GoogleFonts.poppins(fontSize: 16),
-            shape: getOrientation(),
-          ),
-          child: Text(
-            optionText,
-            style: const TextStyle(color: Colors.black),
-          )),
+        onPressed: () {
+          final source =
+              optionText == 'Camera' ? ImageSource.camera : ImageSource.gallery;
+          _setPickedImage(context, source);
+          Navigator.pop(context);
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: appPainter.getDialogOptionColor(),
+          textStyle: GoogleFonts.poppins(fontSize: 16),
+          shape: getOrientation(),
+        ),
+        child: Text(
+          optionText,
+          style: const TextStyle(color: Colors.black),
+        ),
+      ),
     );
   }
 }
