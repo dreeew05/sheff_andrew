f8e73153d8a77ccee608cf6e945b56bfc27b777f
Glen Andrew <gcbulaong@up.edu.ph>
2024-05-25 02:04:33 +0800

    Delete interface

--- lib/providers/interfaces/image_picker_provider_interface.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:image_picker/image_picker.dart';
-
-abstract class ImagePickerProviderInterface {
-  File? get selectedImage;
-  Future setPickedImage(ImageSource source);
-}
