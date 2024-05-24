a2da3c80a9cb3293d7387d62906fac539a24be07
Glen Andrew <gcbulaong@up.edu.ph>
2024-05-25 02:06:47 +0800

    Refactor

--- lib/screens/add_recipe/components/recipe_details.dart
+++ lib/screens/add_recipe/components/recipe_details.dart
@@ -2,23 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:sheff_andrew/dialogs/image_picker_dialog.dart';
+import 'package:sheff_andrew/common/components/image_pick.dart';
 import 'package:sheff_andrew/providers/recipe_form_provider.dart';
+
+final _mealTypeEntries = <String>[
+  'Breakfast',
+  'Lunch',
+  'Dinner',
+  'Snack',
+  'Breakfast/Lunch',
+  'Lunch/Dinner'
+];
 
 class RecipeDetails extends StatefulWidget {
   const RecipeDetails({super.key});
 
   @override
-  RecipeDetailsState createState() => RecipeDetailsState();
+  _RecipeDetailsState createState() => _RecipeDetailsState();
 }
 
-class RecipeDetailsState extends State<RecipeDetails> {
+class _RecipeDetailsState extends State<RecipeDetails> {
   @override
   Widget build(BuildContext context) {
     final providerWatcher = context.watch<RecipeFormProvider>();
     final providerReader = context.read<RecipeFormProvider>();
-    final recipeImage = providerWatcher.selectedImage;
-    String mealType = providerReader.mealType;
+    final recipeImage = providerWatcher.recipeImage;
     return Container(
         margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
         child: SingleChildScrollView(
@@ -33,43 +41,7 @@ class RecipeDetailsState extends State<RecipeDetails> {
                 ),
               ),
               const SizedBox(height: 20),
-              Align(
-                alignment: Alignment.center,
-                child: GestureDetector(
-                    onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const ImagePickerDialog();
-                          });
-                    },
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundImage: recipeImage != null
-                          ? FileImage(recipeImage!)
-                          : null, // No background if recipeImage is null
-                      child: recipeImage == null
-                          ? const Icon(Icons.add_photo_alternate_outlined)
-                          : null, // Add the icon if no image is loaded
-                    )),
-              ),
-              const SizedBox(height: 5),
-              Align(
-                alignment: Alignment.center,
-                child: TextButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return const ImagePickerDialog();
-                        });
-                  },
-                  child: Text(
-                    'Add Image',
-                    style: GoogleFonts.poppins(),
-                  ),
-                ),
-              ),
+              ImagePick(image: recipeImage, imageType: 'recipe'),
               const SizedBox(height: 10),
               TextFormField(
                 controller: providerReader.recipeNameController,
@@ -93,25 +65,15 @@ class RecipeDetailsState extends State<RecipeDetails> {
                 // This is not our fault.
                 // Shit moves the tabView when clicked
                 child: DropdownMenu<String>(
+                  controller: providerReader.mealTypeController,
                   textStyle: GoogleFonts.poppins(),
                   label: Text(
                     "Meal Type",
                     style: GoogleFonts.poppins(fontSize: 15),
                   ),
-                  initialSelection: mealType,
-                  onSelected: (String? value) {
-                    setState(() {
-                      mealType = value!;
-                    });
-                  },
-                  dropdownMenuEntries: <String>[
-                    'Breakfast',
-                    'Lunch',
-                    'Dinner',
-                    'Snack',
-                    'Breakfast/Lunch',
-                    'Lunch/Dinner'
-                  ].map<DropdownMenuEntry<String>>((String value) {
+                  initialSelection: _mealTypeEntries[0],
+                  dropdownMenuEntries: _mealTypeEntries
+                      .map<DropdownMenuEntry<String>>((String value) {
                     return DropdownMenuEntry(
                       value: value,
                       label: value,
@@ -156,5 +118,3 @@ class RecipeDetailsState extends State<RecipeDetails> {
         ));
   }
 }
-
-class _mealType {}
