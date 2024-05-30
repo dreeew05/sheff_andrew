9fd8751c57ee4398645e84e779d7095f103e3867
Glen Andrew <gcbulaong@up.edu.ph>
2024-05-30 17:04:15 +0800

    Refactor pages
    - Added gradient to picture for contrast
    - Added capability to use default image
    - Changed font family

--- lib/common/components/network_image_check.dart
+++ lib/common/components/network_image_check.dart
@@ -1,46 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-// class NetworkImageCheck extends StatefulWidget {
-//   final String url;
-//   final String defaultImageURL;
-
-//   const NetworkImageCheck(
-//       {super.key, required this.url, required this.defaultImageURL});
-
-//   @override
-//   _NetworkImageCheckState createState() => _NetworkImageCheckState();
-// }
-
-// class _NetworkImageCheckState extends State<NetworkImageCheck> {
-//   String _finalImageLink = '';
-//   @override
-//   void initState() {
-//     super.initState();
-//     _checkImageURL();
-//   }
-
-//   Future<void> _checkImageURL() async {
-//     final response = await http.head(Uri.parse(widget.url));
-//     String finalImageLink =
-//         response.statusCode == 200 ? widget.url : widget.defaultImageURL;
-//     setState(() {
-//       _finalImageLink = finalImageLink;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return CircleAvatar(
-//       radius: 100,
-//       backgroundImage: NetworkImage(_finalImageLink),
-//     );
-//   }
-// }
 
 class NetworkImageCheck {
   Future<String> checkImageURL(String url, String defaultImage) async {
-    final response = await http.head(Uri.parse(url));
+    final response = await http.get(Uri.parse(url));
     return response.statusCode == 200 ? url : defaultImage;
   }
 }
